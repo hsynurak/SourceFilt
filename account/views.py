aa6582701db06bd8336091cbbc5dd6b654edf407
huseyinurak <huseyinurak@gmail.com>
2024-06-27 16:18:39 +0300

    backend update
    account pages

--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-
+from django.conf import settings
+from django.core.mail import send_mail
 
 # Create your views here.
 def login_request(request):
@@ -10,7 +11,6 @@ def login_request(request):
         if request.method == "POST":
           username = request.POST.get('username')
           password = request.POST.get('password')
-          
           user = authenticate(request, username=username, password=password)
           if user is not None:
                login (request, user)
@@ -22,34 +22,34 @@ def login_request(request):
 
 
 def password_request(request):
-        return render(request, "account/password.html")
+     return render(request, "account/password.html")
 
 
 def signup_request(request):
         if request.user.is_authenticated:
           return redirect("index")
+     
         if request.method == "POST":
           username = request.POST["username"]
           email = request.POST["email"]
-          firstname = request.POST["firstname"]
-          lastname = request.POST["lastname"]
           password = request.POST["password"]
           repassword = request.POST["repassword"]
           
           if password == repassword:
                if User.objects.filter(username=username).exists():
-                    return render(request, "account/register.html", {"error":"Kullanıcı adı kullanılıyor"})
+                    return render(request, "account/signup.html", {"error":"Kullanıcı adı kullanılıyor"})
                else:
                     if User.objects.filter(email=email).exists():
-                         return render(request, "account/register.html", {"error":"Email kullanılıyor"})
+                         return render(request, "account/signup.html", {"error":"Email kullanılıyor"})
                     else:
-                         user = User.objects.create_user(username=username, email=email, password=password, first_name=firstname, last_name=lastname)
+                         user = User.objects.create_user(username=username, email=email, password=password)
                          user.save()
                          return redirect("login")
           else:
-               return render(request, "account/register.html", {"error":"Şifreler uyuşmuyor"})
+               return render(request, "account/signup.html", {"error":"Şifreler uyuşmuyor"})
            
         return render(request, "account/signup.html")
    
 def logout_request(request):
+     logout(request)
      return redirect("index")
--- sourcefilt/settings.py
+++ sourcefilt/settings.py
@@ -124,7 +124,20 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+#EMAIL
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sourcefiltteknik@gmail.com'
+EMAIL_HOST_PASSWORD = 'sourcefilt123.'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
